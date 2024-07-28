dd5e19ef6d4e26eae98d2b84e91266f3b62bd204
danielkingori <kingndiritu@gmail.com>
2024-07-28 23:19:03 +0300

    show

--- task_4.sql
+++ task_4.sql
@@ -2,6 +2,13 @@
 USE alx_book_store;
 
 -- Get the table structure as a string
-SELECT CONCAT("DESCRIBE `", table_name, "`;") AS stmt
-FROM information_schema.tables
+SELECT CONCAT(
+    "Column_name: ", COLUMN_NAME, "\n",
+    "Type: ", DATA_TYPE, "\n",
+    "Is_nullable: ", IS_NULLABLE, "\n",
+    "Column_key: ", COLUMN_KEY, "\n",
+    "Column_default: ", COLUMN_DEFAULT, "\n",
+    "Extra: ", COLUMN_TYPE, "\n\n"
+) AS stmt
+FROM information_schema.columns
 WHERE table_name = 'books' AND table_schema = DATABASE();
