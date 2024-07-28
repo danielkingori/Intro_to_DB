05df6b967878e5d6c07574d99f7e7036574e499f
danielkingori <kingndiritu@gmail.com>
2024-07-28 23:36:20 +0300

    instert

--- task_6.sql
+++ task_6.sql
@@ -5,17 +5,17 @@ USE alx_book_store;
 SET @customer_id_1 = 2;
 SET @customer_name_1 = 'Blessing Malik';
 SET @email_1 = 'bmalik@sandtech.com';
-SET @address_1 = '124 Happiness Ave.';
+SET @address_1 = '124 Happiness  Ave.';
 
 SET @customer_id_2 = 3;
 SET @customer_name_2 = 'Obed Ehoneah';
 SET @email_2 = 'eobed@sandtech.com';
-SET @address_2 = '125 Happiness Ave.';
+SET @address_2 = '125 Happiness  Ave.';
 
 SET @customer_id_3 = 4;
 SET @customer_name_3 = 'Nehemial Kamolu';
 SET @email_3 = 'nkamolu@sandtech.com';
-SET @address_3 = '126 Happiness Ave.';
+SET @address_3 = '126 Happiness  Ave';
 
 -- Insert the rows
 INSERT INTO customer (customer_id, customer_name, email, address)
